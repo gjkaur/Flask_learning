20b195278d662684582e3e4589b091b58d0af991
Gurinderjeet Kaur <36306330+gjkaur@users.noreply.github.com>
2023-09-23 00:17:40 +0530

    second program

--- Program_2.py
+++ Program_2.py
@@ -2,10 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/sum/<x>/<y>")
-def sum():
+def sum(x,y):
     sum = x+y
     return {"Result":sum}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=50100, debug=True)
-    
